54697c50078561081289fe0d75a8daecbdf65516
a1im <a1imov233@gmail.com>
2016-11-09 15:24:50 +0300

    update

--- index.php
+++ index.php
@@ -1,7 +1,43 @@
+<!-- <?php
+	echo phpinfo();
+?> -->
+
 <html>
 	<head>
 	    <meta charset="utf-8">
 		<title>Карта</title>
+		<style>
+		#gradient {
+			position: absolute;
+			top: 50%;
+			right: 10px;
+			background: #eee;
+			-moz-transform: translateY(-50%);
+			-webkit-transform: translateY(-50%);
+			-o-transform: translateY(-50%);
+			-ms-transform: translateY(-50%);
+			transform: translateY(-50%);
+			z-index: 9999;
+		}
+		#gradient p {
+			margin: 15px 25px 10px 10px;
+			text-align: right;
+		}
+		#gradient .gradient {
+			position: absolute;
+			top: 0;
+			right: 0;
+			width: 20px;
+			height: 100%;
+		    background: #00ff00;
+		    background: -moz-linear-gradient(top, #ffffff 0%, #00ff00 100%);
+		    background: -webkit-linear-gradient(top, #ffffff 0%, #00ff00 100%);
+		    background: -o-linear-gradient(top, #ffffff 0%, #00ff00 100%);
+		    background: -ms-linear-gradient(top, #ffffff 0%, #00ff00 100%);
+		    background: linear-gradient(top, #ffffff 0%, #00ff00 100%);
+
+		}
+		</style>
 	</head>
 	<body>
 	    <p id=info></p>
@@ -10,6 +46,16 @@
 	        <label>До: </label> <input type=date name=date2 value="2016-06-02" min="1950-01-01" />
 	        <button type=submit>Посмотреть</button>
 	    </form>
+	    <div id="gradient">
+	    	<p>0.1</p>
+	    	<p>5</p>
+	    	<p>10</p>
+	    	<p>15</p>
+	    	<p>20</p>
+	    	<p>25</p>
+	    	<p>30</p>
+	    	<div class="gradient"></div>
+	    </div>
 		<div id=map style="width: 100%; height: 100%"></div>
 	
 <!--   		<img src="createImg/png.php?tileX=0&tileY=0&tileZ=2" />
